91cc57f0ed8272f0c4119f31eb7ee6372980debf
eto <eteri_ivanelashvili@iliauni.edu.ge>
2022-03-25 14:30:22 +0400

    somenew

--- screens/auth/HomeScreen.tsx
+++ screens/auth/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {
   Image,
   ScrollView,
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  FlatList,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {ScreenNavigationProp} from '../../interfaces/commons';
@@ -16,12 +17,27 @@ import {use} from '../../Store/actions/translate';
 import {ITranslateReducer, ITranslateState} from '../../Store/types/translate';
 import Colors from '../../theme/Colors';
 import LinearGradient from 'react-native-linear-gradient';
+import DATA from '../../constants/shopListDummyData';
+import ShopingCard from '../../components/ShopCard';
 
 const HomeScreen: React.FC<ScreenNavigationProp> = props => {
   const dispatch = useDispatch();
+  const renderItem = useCallback(({item}) => {
+    console.log('essssssssssi', item);
+    return (
+      <View>
+        <ShopingCard {...item} />
+      </View>
+    );
+  }, []);
+
   const translateReducer = useSelector<ITranslateReducer>(
     state => state.TranslateReducer,
   ) as ITranslateState;
+
+  const keyExtractor = useCallback(item => {
+    return item.id;
+  }, []);
 
   const signin = () => {
     AuthService.SignIn({email: 'fhjdskhfjd', password: 'fdsfds'}).subscribe({
@@ -44,6 +60,13 @@ const HomeScreen: React.FC<ScreenNavigationProp> = props => {
           style={styles.img}
           source={require('../../assets/img/cardGreen.png')}
         />
+        <View style={styles.markView}>
+          <Image
+            style={styles.mark}
+            source={require('../../assets/img/UniMark.png')}
+          />
+          <Text style={styles.amount}>54000</Text>
+        </View>
       </View>
       <View style={styles.linearView}>
         <LinearGradient
@@ -51,6 +74,39 @@ const HomeScreen: React.FC<ScreenNavigationProp> = props => {
           colors={[Colors.gradiantDark, Colors.gradiantLight, Colors.bgColor]}
         />
       </View>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>რაში დავხარჯო</Text>
+      </View>
+      <View style={styles.flatlist}>
+        <FlatList
+          contentContainerStyle={{
+            alignSelf: 'flex-start',
+          }}
+          numColumns={DATA.length / 2}
+          data={DATA}
+          renderItem={renderItem}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={keyExtractor}
+        />
+      </View>
+
+      {/* <ScrollView
+        horizontal={true}
+        pagingEnabled={true}
+        style={{flex: 1}}
+        contentContainerStyle={{
+          flex: 1,
+          flexWrap: 'wrap',
+          // flexDirection: 'row',
+          justifyContent: 'space-evenly',
+        }}>
+        {DATA.map((data, i) => (
+          <View key={i}>
+            <ShopingCard {...data} />
+          </View>
+        ))}
+      </ScrollView> */}
 
       <Text>{translateReducer.t('common.name')}</Text>
       <TouchableOpacity
@@ -76,12 +132,39 @@ const styles = StyleSheet.create({
     width: 289.14,
     height: 187,
   },
+  markView: {
+    position: 'absolute',
+    left: 35,
+    bottom: -18,
+    alignItems: 'center',
+  },
+  mark: {
+    width: 93,
+    height: 93,
+  },
+  amount: {
+    color: Colors.amountTxt,
+    fontSize: 28,
+  },
   linearView: {
     width: '100%',
-    marginTop: 30,
+    marginTop: 38,
   },
   linear: {
     height: 20,
+  },
+  titleWrapper: {
+    marginHorizontal: 46,
+    marginTop: 35,
+  },
+  title: {
+    fontSize: 14,
+    textTransform: 'uppercase',
+    color: Colors.black,
+    fontWeight: '400',
+  },
+  flatlist: {
+    marginTop: 28,
   },
 });
 
